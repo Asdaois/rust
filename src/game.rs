2a1a673bfddca4765cc06fd1cfeded8373349189
Jose Guevara <94gjose@gmail.com>
2023-09-24 12:36:00 -0400

    feat: show basic screen

--- src/game.rs
+++ src/game.rs
@@ -1,8 +1,15 @@
 mod init;
+static THICKNESS: u32 = 15;
+static PADDLE_H: f32 = 100.0;
+static WIDTH: u32 = 480;
+static HEIGHT: u32 = 480;
+
+use std::os::fd::IntoRawFd;
 
 use sdl2::{
     keyboard::Scancode,
     pixels::Color,
+    rect::Rect,
     render::Canvas,
     sys::{SDL_DestroyWindow, SDL_Quit},
     video::Window,
@@ -52,7 +59,36 @@ impl Game {
     fn update_game(&self) {}
     fn generate_output(&mut self) {
         self.canvas.set_draw_color(Color::RGBA(0, 0, 255, 255));
+
         self.canvas.clear();
+
+        self.canvas.set_draw_color(Color::RGBA(255, 255, 255, 255));
+
+        self.canvas
+            .fill_rect(Rect::new(0, 0, WIDTH, THICKNESS))
+            .unwrap();
+
+        self.canvas
+            .fill_rect(Rect::new(0, (HEIGHT - THICKNESS) as i32, WIDTH, THICKNESS))
+            .unwrap();
+
+        self.canvas
+            .fill_rect(Rect::new((WIDTH - THICKNESS) as i32, 0, THICKNESS, HEIGHT))
+            .unwrap();
+
+        self.canvas
+            .fill_rect(Rect::new(
+                (THICKNESS / 2).try_into().unwrap(),
+                (HEIGHT / 2).try_into().unwrap(),
+                THICKNESS,
+                50,
+            ))
+            .unwrap();
+
+        self.canvas
+            .fill_rect(Rect::new(240, 240, THICKNESS, THICKNESS))
+            .unwrap();
+
         self.canvas.present();
     }
 }
