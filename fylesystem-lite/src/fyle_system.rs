a60d934f0e6b056f87ad62a435bf1988c4cb9fa7
Jose Guevara <94gjose@gmail.com>
2023-10-07 11:07:10 -0400

    feat: dummit file system

---
+++ fylesystem-lite/src/fyle_system.rs
@@ -0,0 +1,12 @@
+pub type File = String;
+
+pub fn open(file: &mut File) -> bool {
+    true
+}
+pub fn close(file: &mut File) -> bool {
+    true
+}
+
+pub fn read(file: &mut File, save_to: &mut Vec<u8>) -> ! {
+    todo!()
+}
