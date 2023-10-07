a207119306dcc78a99c5b7cece9f46f62aa2395f
Jose Guevara <94gjose@gmail.com>
2023-10-07 11:07:10 -0400

    feat: basic of tests

--- src/area_rect.rs
+++ src/area_rect.rs
@@ -3,3 +3,18 @@ use std::ops::Mul;
 pub fn area_rect<T: Mul<Output = T>>(length: T, width: T) -> T {
   length * width
 }
+
+#[cfg(test)]
+mod test {
+  use crate::area_rect::area_rect;
+
+  #[test]
+  fn test_area_rect_int() {
+    assert_eq!(area_rect(4, 5), 20);
+  }
+
+  #[test]
+  fn test_area_rect_float() {
+    assert_eq!(area_rect(4., 5.), 20.);
+  }
+}
