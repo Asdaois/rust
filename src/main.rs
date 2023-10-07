02b66a4ae71d4cab36cc1dd1deeb5d563f7a6f9e
Jose Guevara <94gjose@gmail.com>
2023-10-07 11:07:10 -0400

    feat: basic control flow

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ fn greet(name: &str) {
   println!("Hello, {name}!");
 }
 
+// function_name(<argument values: type>)
+fn sum(x: i32, y: i32) -> i32 {
+  println!("Calculating sum..."); // Statement
+  x + y // Expression, return a value
+}
+
 fn main() {
   // Immutable variable
   let name = "World";
@@ -58,4 +64,45 @@ fn main() {
   // Panic index out of bound
   // println!("last element is = {}", arr[len]);
   println!("last element is = {}", arr[len - 1]);
+
+  println!("The sum of 7 and 5 is: {}", sum(7, 5));
+
+  let x = 6;
+  if x == 5 {
+    println!("X is 5");
+  } else if x == 9 {
+    println!("X is 9");
+  } else {
+    println!("X is neither 5 or 9")
+  }
+
+  let mut x = 0;
+  loop {
+    println!("X -> {x}");
+    x += 1;
+
+    if x == 3 {
+      break;
+    }
+  }
+
+  // Return value with loop
+  let mut x = 1;
+  let mut i = 0;
+  let result = loop {
+    println!("Get ${x} or double and give it to the next person");
+    x += x;
+
+    i += 1;
+    if i > 5 {
+      break x;
+    }
+  };
+
+  println!("Bankrupt I need to give ${result}");
+
+  let array = [10, 20, 30, 40, -1];
+  for number in array {
+    println!("val = {number}");
+  }
 }
