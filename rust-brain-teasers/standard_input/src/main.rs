7ec29afa0f6a54af6593c250347d21a8e246d60c
Jose Guevara <94gjose@gmail.com>
2023-10-10 08:12:59 -0400

    feat: float imperfection and weird input

---
+++ rust-brain-teasers/standard_input/src/main.rs
@@ -0,0 +1,18 @@
+use std::io::stdin;
+
+fn main() {
+    println!("what is 3 + 2? Type your answer and press enter");
+    let mut input = String::new();
+
+    stdin()
+        .read_line(&mut input)
+        .expect("Unable to read standard input");
+
+    if input.trim() == "5" {
+        println!("Correct!")
+    } else {
+        println!("Incorrect!")
+    }
+
+    println!("{:#?}", input)
+}
